67dfbd979a75501448767cfd13d9a2534bd6a850
Md Mostafijur Rahman <104758273+cadmostafijur@users.noreply.github.com>
2025-04-14 15:29:23 +0600

    Update mycode.asm
    Task Title: Check If a Number Is Divisible by
    Both 5 and 11

--- mycode.asm
+++ mycode.asm
@@ -78,4 +78,48 @@ MAIN PROC
     int 21h
 MAIN ENDP
 END MAIN
+ Task Title: Check If a Number Is Divisible by
+Both 5 and 11     
+.MODEL SMALL
+.STACK 100H
+.DATA 
+A DB 13,10,"THIS IS NOT DIVISIBLE BY BOTH 5 AND 11$"
+B DB   13 ,10 , " THIS IS DIVISIBLE BY BOTH 5 AND?11 $"
+.CODE
+MAIN PROC
+    Mov AX,@Data
+    Mov DS ,AX  
+    
+    MOV AX, 55
+    MOV CX, AX
+    
+    MOV BL, 5
+    DIV BL
+    
+    CMP AH, 0
+    
+    JNE Not_divisiable
+    
+    MOV AX,CX
+    MOV BL ,12
+    DIV BL
+    CMP AH ,0  
+    JNE Not_divisiable
+    
+    LEA DX, B
+    MOV AH,9
+    INT 21H
+    JMP EXIT
+Not_divisiable:
+    LEA DX , A
+    MOV AH , 9
+    INT 21H
+EXIT:
+MOV AH, 4CH
+INT 21H
+
+    
+
+MAIN ENDP
+END MAIN
       
